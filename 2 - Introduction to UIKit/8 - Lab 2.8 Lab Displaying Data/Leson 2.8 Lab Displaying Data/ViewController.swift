52a267b01b52b2ecca6635c873f7dac74fb9e7e8
Doug <42744099+DouglasPatterson801@users.noreply.github.com>
2018-11-28 16:05:39 -0700

    Add files via upload

---
+++ 2 - Introduction to UIKit/8 - Lab 2.8 Lab Displaying Data/Leson 2.8 Lab Displaying Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leson 2.8 Lab Displaying Data
+//
+//  Created by Douglas Patterson on 9/19/18.
+//  Copyright © 2018 Douglas Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
